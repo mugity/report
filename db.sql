74e4f390718424b359710f9e838c5f8f0c1109f0
mugity <49706130+mugity@users.noreply.github.com>
2019-07-03 13:45:18 +0900

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-create table class ( id int auto_increment not null primary key, start_class varchar(100), usable_weapons varchar(100), class_change varchar(100), add_weapon varchar(100), type int );
+create table class ( id int auto_increment not null primary key, start_class varchar(100), weapons varchar(100), class_change varchar(100), type int );
 create table status ( id int auto_increment not null primary key, Lv int, HP int, str int, skl int, spd int, lck int, def int, res int, con int, mov int );
 create table growth_rate ( id int auto_increment not null primary key, HP_rate int, str_rate int, skl_rate int, spd_rate int, lck_rate int, def_rate int, res_rate int );
 create table units ( id int auto_increment not null primary key, name varchar(100), class_id int, status_id int, growth_id int );
