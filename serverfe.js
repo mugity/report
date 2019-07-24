eefd736271ad8057e339e467ecbc6d4553448076
mugity <49706130+mugity@users.noreply.github.com>
2019-07-24 13:53:15 +0900

    Update serverfe.js

--- serverfe.js
+++ serverfe.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 });
 
 server.get('/', function( req, res ){
-    let query = 'select units.id, units.name, class.start_class from units inner join class on units.class_id = class.id desc limit 10;';
+    let query = 'select units.id, units.name, class.start_class from units inner join class on units.class_id = class.id order by units.name desc limit 10;';
     console.log( query );
     connection.query( query, (error, rows, fields) => {
         if( error ) {
