a4c3756b3f7e8e8d8021c5079a63bf01ba9032a1
mugity <49706130+mugity@users.noreply.github.com>
2019-07-31 23:36:20 +0900

    Update serverfe.js

--- serverfe.js
+++ serverfe.js
@@ -37,7 +37,7 @@ server.get('/status', function( req, res ){
 });
 
 server.get('/growth', function( req, res ){
-    let growsort = req.query.growsort || 'HP';
+    let growsort = req.query.growsort || 'HP_rate';
     let grownumber = req.query.grownumber || 10;
     let query3 = 'select units.id, units.name, growth_rate.HP_rate, growth_rate.str_rate, growth_rate.skl_rate, growth_rate.spd_rate, growth_rate.lck_rate, growth_rate.def_rate, growth_rate.res_rate from units inner join growth_rate on units.growth_id = growth_rate.id order by growth_rate.' + growsort + ' desc limit ' + grownumber + ';';
     console.log( query3 );
